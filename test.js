1538f1ec3926a56de6ebd3080a7e8b5908de1b67
Dutta <riya.a.dutta@capgemini.com>
2018-03-13 16:18:30 +0530

    added js

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 //Test line
 //tsettsets
 // asdasdasd - ha hahah ahahhah - xxxxxxxxxxxxxxxx - Hori ka patol
+//Hello JS .. Let's do it! --Changes made by Riya--
+<script>
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+</script>
