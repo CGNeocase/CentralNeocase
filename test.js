772d2304b864df0d1740967f046ec08d2873a231
Dutta <riya.a.dutta@capgemini.com>
2018-03-13 16:48:22 +0530

    V 1

--- test.js
+++ test.js
@@ -4,8 +4,11 @@
 //Hello JS .. Let's do it! --Changes made by Riya--
 <script>
 document.getElementById("demo").innerHTML = "Hello JavaScript!";
-</script>
+document.getElementById("demo").style.fontSize = "25px";
+document.getElementById("demo").style.color = "red";
+document.getElementById("demo").style.backgroundColor = "yellow";
 //--Changes made by Riya BranchRiya--
 typeof "5";
 </script>
+
 
