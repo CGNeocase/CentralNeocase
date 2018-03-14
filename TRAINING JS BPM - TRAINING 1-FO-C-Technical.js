9679f793c3ae5365314b7a2675a338236b1ce873
Khan <md.c.khan@capgemini.com>
2018-03-14 13:32:06 +0530

    version 6

--- TRAINING JS BPM - TRAINING 1-FO-C-Technical.js
+++ TRAINING JS BPM - TRAINING 1-FO-C-Technical.js
@@ -1,1 +1,24 @@
 //Md Khan
+var startDate = new Date();
+var startTime = startDate.getTime();
+
+
+neocase.form.event.bind("loadcomplete",function(){
+var difference = (new Date()).getTime() - startTime; 
+console.warn("loadcomplete : "+difference);
+
+});
+
+
+
+neocase.form.event.bind("init",function(){
+var difference = (new Date()).getTime() - startTime; 
+console.warn("Init : "+difference);
+});
+
+
+neocase.form.event.bind("preinit",function(){
+var difference = (new Date()).getTime() - startTime; 
+console.warn("Preinit : "+difference);
+
+});
